c9e213c0ad4e7b47af12af0aa63d17e40eb58de6
AnushaPuttaswamy <149838433+AnushaPuttaswamy@users.noreply.github.com>
2023-11-20 10:49:44 +0530

    test_bench_load_L_type.sv

--- test_bench_load_L_type.sv
+++ test_bench_load_L_type.sv
@@ -10,23 +10,19 @@ module tb_L_type;
     logic [31:0] out;
 
     initial begin
-        // Initialize inputs
         instr = 3'b010; // LW operation
         daddr = 32'h00000008; // Address offset = 8
         drdata = 32'hA5A5A5A5;
 
-        // Apply inputs to the module
         #10 instr = 3'b001; // LH operation
         #10 instr = 3'b100; // LBU operation
-        #10 instr = 3'b011; // Unsupported operation, default case
+        #10 instr = 3'b011; //default case test
 
-        // Monitor outputs
         #10 $display("out = %h", out);
-        #10 $finish; // Finish simulation after a delay
+        #10 $finish; 
     end
 
-    // Instantiate the module
-    L_type uut (
+     L_type uut (
         .instr(instr),
         .daddr(daddr),
         .drdata(drdata),
