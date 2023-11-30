f7b866e42f79318bf7563c72a4a9824e15867854
ashwinkumar-sivakumar <150288839+ashwinkumar-sivakumar@users.noreply.github.com>
2023-11-30 00:31:01 -0800

    Update intf.sv
    converting the imem using interface and modports

--- rtl/intf.sv
+++ rtl/intf.sv
@@ -1,16 +1,24 @@
 `timescale 1ns / 1ps
 interface intf;
-
+//R_type parameters
 logic [31:0] instr;
 logic signed [31:0] in1;
 logic signed [31:0] in2;
 logic [31:0] out;
- 
+//Imem parameters
+logic [31:0] iaddr;
+logic [31:0] idata;
+
 modport r_type (
     input  instr,
     input  in1,
     input  in2,
     output out
+);
+
+modport i_mem(
+    input iaddr,
+    output idata
 );
 
 endinterface
